631b3cdf9e8e242a70544d6b415bb13864200dbf
Hanna Bennett <bennetthanna22@gmail.com>
2019-01-30 21:28:34 -0800

    Promisify AWS requests

--- generators/app/index.js
+++ generators/app/index.js
@@ -61,25 +61,24 @@ module.exports = class extends Generator {
             RoleArn: 'arn:aws:iam::171578128461:role/full-s3-access',
             RoleSessionName: 'MoosicSession'
         }
-        sts.assumeRole(assumeRoleParams, (err, data) => {
-            if (err) {
-                console.log(`Whoopsie daisies! Looks like an error occured when trying to assume role: ${err.message}`);
-                return done();
-            }
-            const assumedRoleCredentials = {
-                accessKeyId: data.Credentials.AccessKeyId,
-                secretAccessKey: data.Credentials.SecretAccessKey,
-                sessionToken: data.Credentials.SessionToken
-            };
-            const s3 = new AWS.S3(assumedRoleCredentials);
-            s3.listObjectsV2({ Bucket: 'bucket-o-luv', MaxKeys: 2 }, (err, data) => {
-                if (err) {
-                    console.log(`Uh oh spaghettios! Looks like an error occured when trying to list objects: ${err.message}`);
-                    return done();
-                }
-                console.log(data);
+
+        const assumeRole = sts.assumeRole(assumeRoleParams).promise();
+
+        assumeRole
+            .then(res => {
+                const assumedRoleCredentials = {
+                    accessKeyId: res.Credentials.AccessKeyId,
+                    secretAccessKey: res.Credentials.SecretAccessKey,
+                    sessionToken: res.Credentials.SessionToken
+                };
+                const s3 = new AWS.S3(assumedRoleCredentials);
+                return s3.listObjectsV2({ Bucket: 'bucket-o-luv', MaxKeys: 2 }).promise();
+            })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
             });
-            return done();
-        });
     }
 };
